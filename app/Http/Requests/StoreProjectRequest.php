6808bdf16010691321f42ff0c8da34a529921987
GianmauroCoviello <gianmaurocoviello@gmail.com>
2023-08-29 10:24:49 +0200

    continued project

---
+++ app/Http/Requests/StoreProjectRequest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreProjectRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'title'=>'required|max:50',
+            'cover_image'=>'image|max:10000',
+            'type_id' => 'required|exists:types,id'
+
+
+        ];
+
+
+    }
+
+
+    // nuova funzione chiave valore per mettere il messaggio di errore in italiano
+    public function messages()
+    {
+        return [
+
+            'title.required'=>'il titolo e obbligatorio',
+            'title.max'=>'ERRORE: il titolo supera il limite di caratteri consentito (50)',
+            'cover_image.image'=>'ERRORE: assicurati che il file caricato sia in formato png, jpeg, jpg',
+            'type_id.required'=>'Devi selezionare una tipologia',
+            'type_id.exist'=>'tipologia selezionata non valida',
+
+
+        ];
+
+
+    }
+
+    
+}
